a595edb18b33d05ca191ef9d8975c28e7dce0359
Boris <rozumniy.boris.work@gmail.com>
2021-02-25 11:26:16 +0200

    init backend

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(
+  express.json({ extended: true })
+);
+
+// app.use(
+//   "/api/auth",
+//   require("./routes/auth.routes")
+// );
+// app.use(
+//   "/api/products",
+//   require("./routes/product.routes")
+// );
+
+const PORT = config.get("port") || 5080;
+
+async function start() {
+  try {
+    await mongoose.connect(
+      config.get("mongoUri"),
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+    app.listen(PORT, () =>
+      console.log(
+        `App has been starder on port ${PORT}...`
+      )
+    );
+  } catch (e) {
+    console.log(
+      "Server Error",
+      e.message
+    );
+    process.exit(1);
+  }
+}
+
+start();
