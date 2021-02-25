074cebd9cd565174ffacaec0348689b9cd86f606
Boris <rozumniy.boris.work@gmail.com>
2021-02-25 21:31:30 +0200

    applay the post Todos router on the backend

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,23 +1,37 @@
-const {Router} = require('express')
-const config = require('config')
-const Product = require('../models/Product')
-const router = Router()
+const { Router } = require("express");
+const config = require("config");
+const Todo = require("../models/Todo");
+const router = Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   try {
-    res.json({test: 'test'})
+    res.json({ test: "test" });
   } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'})
+    res.status(500).json({
+      message:
+        "Что-то пошло не так, попробуйте снова",
+    });
   }
-})
+});
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    console.log(req);
-    res.send('POST request to the homepage', req);
+    const todos = req.body;
+    todos.forEach((todo) => {
+      const newTodo = new Todo(todo);
+      newTodo.save();
+    });
+    res
+      .status(201)
+      .send(
+        "POST request to the todos"
+      );
   } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'})
+    res.status(500).json({
+      message:
+        "Что-то пошло не так, попробуйте снова",
+    });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
